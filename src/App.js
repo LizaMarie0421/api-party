86373619a95d049adfa07a284d4536ee0a656ff1
LizaMarie <lizamariejimenez@gmail.com>
2017-07-11 15:01:51 -0500

    adding github component with styling also with router before default

--- src/App.js
+++ src/App.js
@@ -1,24 +1,32 @@
 import React, { Component } from 'react';
-import { Route, Switch } from 'react-router-dom'
 import './App.css';
+import {Route, Switch, NavLink} from 'react-router-dom' 
+import Github from './Github'
+import Nasa from './Nasa'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <div className="App-headings">
-            <h3>Ain't no party like an</h3>
-            <h1>API Party</h1>
+          <div className='App-headings'> 
+            <h3>Ain't no party like an </h3>
+            <h1>API Party </h1>
           </div>
+          <ul className="nav-links">
+          <li>
+            <NavLink to='/github'>Github API</NavLink>
+            <NavLink to='/Nasa'>  Nasa API </NavLink>
+            </li>
+          </ul>
         </div>
         <Switch>
-          <Route render ={()=> <p>To get started, click one of the links above</p>}/>
-
-        </Switch>  
+          <Route path='/github' component={Github}/>
+          <Route path='/Nasa' component={Nasa}/>
+          <Route render={() => <p>To get started, click one of the links above</p>}/>
+          </Switch>
       </div>
     );
   }
 }
-
 export default App;
